34ae3619c2f3c51995d4e7fffc65eb99e97100b3
Lisna Jos <75560124+lisnajos08@users.noreply.github.com>
2022-02-07 15:23:45 +0530

    Add files via upload

---
+++ Qstn1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 14:47:17 2022
+
+@author: sjcet
+"""
+import numpy as np
+
+
+a = np.arange(1, 11, 1)
+print(a)
+first_element = a[:4]
+print(first_element)
+first_element1 = a[5:]
+print(first_element1)
+first_element2 = a[1:7]
+print(first_element2)
